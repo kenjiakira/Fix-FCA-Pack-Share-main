d5fd84003a8ba0abb24f89ddc6deac724ab2faec
DevNinja <kenjiakira2006@gmail.com>
2026-02-20 11:39:03 +0700

    Feat: Update Vanila version

--- dashboard/backend/src/modules/appstate/appstate.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const appStateController = require('./appstate.controller');
-
-const router = express.Router();
-
-router.post('/', (req, res, next) => appStateController.update(req, res, next));
-
-module.exports = router;
-
